1693635d859422ec1e666e17ed604e11c91df96e
n3r0t <g4llet@gmail.com>
2021-02-16 18:36:08 +0100

    fixed bot sending not replying when there was comment

--- bot/main.py
+++ bot/main.py
@@ -2,11 +2,13 @@ import api
 from errors import *
 from imgsearch import imgSearch
 
+source_list = ["twitter", "pixiv"]
+
+subreddit = 'sleepobeepo'
+
 bot = api.reddit()
 
 footer = "\n\n\n\n ^this ^message ^was ^sent ^[automatically](https://github.com/n3r0t/sleepobeepo)"
-
-source_list = ["twitter", "pixiv"]
 
 
 def streamSubmissions(subreddit: str) -> None:
@@ -21,23 +23,26 @@ def streamSubmissions(subreddit: str) -> None:
     for submission in sleepobeepo.stream.submissions(skip_existing=True):
         print(f"Checking {submission.title} -- {submission.permalink}\n")
         if submission.is_robot_indexable:
-            print(submission)
+            _subid = submission.id
             if not submission.comments.list():
                 print("No comment. Searching for source.\n")
                 try:
-                    bot.submission(submission.id).reply(f'Source: {imgSearch(submission.url)} {footer}')
+                    bot.submission(_subid).reply(f'Source: {imgSearch(submission.url)} {footer}')
                 except NoSourceFound:
-                    bot.submission(submission.id).reply(f'No source found.{footer}')
+                    bot.submission(_subid).reply(f'No source found.{footer}')
             for comment in submission.comments:
                 if any(x in comment.body.lower() for x in source_list):
                     print("Source already posted. Ignoring post.\n")
                     break
                 else:
                     print("No source posted. Searching for source.\n")
-                    imgSearch(submission.url)
+                    try:
+                        bot.submission(_subid).reply(f'Source: {imgSearch(submission.url)} {footer}')
+                    except NoSourceFound:
+                        bot.submission(_subid).reply(f'No source found.{footer}')
         else:
             print("Post deleted. Not searching.\n")
 
 
 if __name__ == "__main__":
-    streamSubmissions('sleepobeepo')
+    streamSubmissions(subreddit)
